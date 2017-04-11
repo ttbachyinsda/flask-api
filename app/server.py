e5f2c1bbb57ee13fd99e57283786a1d3dc57f2b4
ttbachyinsda <gfz14@mails.tsinghua.edu.cn>
2017-04-11 21:55:25 +0800

    5

--- app/server.py
+++ app/server.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 import os  
 from flask_login import LoginManager  
 app=Flask(__name__)  
-app.config.from_object('config')  
 db=SQLAlchemy(app)  
 lm = LoginManager()  
 lm.init_app(app)  
